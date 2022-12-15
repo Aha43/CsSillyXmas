c88e9a294d8be140e7c1c9902cb822166a06337e
Arne Halvorsen <arne.halvorsen@gmail.com>
2022-12-15 12:03:31 +0100

    Did xmas 22

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Enumerable.Range(2, 8).Select(e => e * e).ToList().ForEach(e =>
+{
+    var xmasDict = new Dictionary<int, char> { { 4, 'G' }, { 9, 'o' }, { 16, 'd' }, { 25, ' ' }, { 36, 'J' }, { 49, 'u' }, { 64, 'l' }, { 81, '!' } };
+    var l = xmasDict[e];
+    switch (e)
+    {
+        case 9 * 9:
+            Console.WriteLine(xmasDict[e]);
+            break;
+        default:
+            Console.Write(xmasDict[e]);
+            break;
+    }
+});
+
+// > God Jul!
